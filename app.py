a9f36a68fc0e73c8ca18f6180ee728ca79ee36db
Abhash Goyal <abhashgoyal200@gmail.com>
2023-11-08 21:30:55 +0530

    Done With Key

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-
+import os
 import streamlit as st
+import pickle
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 
 # sidebar
 with st.sidebar:
@@ -11,9 +17,13 @@ with st.sidebar:
 
 def main():
     st.header("Chat with PDF ☁️")
+    load_dotenv()
+
 
     # Uploading PDF files
     pdf = st.file_uploader("Upload your PDF here:", type='pdf')
+    # st.write(pdf.name)
+    # load_dotenv()
 
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
@@ -29,7 +39,34 @@ def main():
         chunks = text_splitter.split_text(text=text)
         st.write(chunks)
 
+        # #embeddings
+        embeddings = OpenAIEmbeddings()
+
+        VectorStore= FAISS.from_texts(chunks,embedding=embeddings)
+
+        store_name=pdf.name[:-4]
+        with open(f"{store_name}.pkl","wb") as f:
+            pickle.dump(VectorStore,f)
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl","rb") as f:
+                VectorStore=pickle.load(f)
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore= FAISS.from_texts(chunks,embedding=embeddings)
+            with open(f"{store_name}.pkl","wb") as f:
+                pickle.dump(VectorStore,f)
         
+    #USER QUESTIONS
+        query = st.text_input("Ask questions On DOCS:")
+    # st.write(query)
+        if query:
+            docs = VectorStore.similarity_search(query=query,k=3)
+            llm=OpenAI(temperature=0,)
+            chain=load_qa_chain(llm=llm,chain_type="stuff")
+            response=chain.run(input_documents=docs,question=query)
+            st.write(response)
+        # st.write(docs)
 
 if __name__ == '__main__':
     main()
